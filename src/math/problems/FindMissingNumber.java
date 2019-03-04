c5ebccd95f8b834739d9e30e12f361710b0838e3
ferhat83 <saadiferhatsaid@gmail.com>
2019-03-03 19:20:34 -0500

    data

--- src/math/problems/FindMissingNumber.java
+++ src/math/problems/FindMissingNumber.java
@@ -11,7 +11,18 @@ public class FindMissingNumber {
          * For example {10, 2, 1, 4, 5, 3, 7, 8, 6}. One number will be missing in array (9 in this case).
          * Write java code to find the missing number from the array. Write static helper method to find it.
          */
-         int [] array = new int[]{10, 2, 1, 4, 5, 3, 7, 8, 6};
+        int[] array = new int[]{10, 2, 1, 4, 5, 3, 7, 8, 6};
+        int missing = 0;
+        for (int i = 0; i < array.length; i++) {
 
+            int x = array[++i];
+            int y = array[i] + 1;
+
+            if (x != y) {
+                missing = y;
+                break;
+            }
+
+        }System.out.println(missing);
     }
 }
--- src/math/problems/LowestNumber.java
+++ src/math/problems/LowestNumber.java
@@ -13,7 +13,13 @@ public class LowestNumber {
 		 * Use one of the databases from mysql or mongodb to store and to retrieve.
 		 */
 		int  array[] = new int[]{211,110,99,34,67,89,67,456,321,456,78,90,45,32,56,78,90,54,32,123,67,5,679,54,32,65};
-
+        int  min = array[0];
+        for(int i=0; i<array.length; i++){
+        	if (array[i]< min){
+        		min = array [i];
+			}
+		System.out.println(min);
+        }
 		//find lowest number from the array
 
 		ConnectToSqlDB connectToSqlDB = new ConnectToSqlDB();
